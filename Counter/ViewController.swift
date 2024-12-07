6370040d3398715b38a69ef4a759ab293419805a
Anastasiia_Bochkovskaia <bnu26111999@gmail.com>
2024-12-07 19:36:21 +0300

    my homework 2nd attempt

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var ButtonCounter: UIButton!
+    @IBOutlet weak var LabelCounter: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func touchedbutton(_ sender: Any) {
+        print("Нажатие")
+        howmuch += 1
+        LabelCounter.text = String(howmuch)
+    }
+    
 
 }
+private var howmuch: Int = 0
 
